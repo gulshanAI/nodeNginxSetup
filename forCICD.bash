501f3623622bde67a845ec98d99b219115754ea0
Gulshan Ramnath Prajapati <35621580+gulshanAI@users.noreply.github.com>
2024-01-23 18:19:37 +0530

    added ci cd

---
+++ forCICD.bash
@@ -0,0 +1,5 @@
+read version
+
+sudo ln -s "$NVM_DIR/versions/node/$version/bin/pm2" "/usr/local/bin/pm2"
+sudo ln -s "$NVM_DIR/versions/node/$version/bin/npm" "/usr/local/bin/npm"
+sudo ln -s "$NVM_DIR/versions/node/$version/bin/node" "/usr/local/bin/node"
